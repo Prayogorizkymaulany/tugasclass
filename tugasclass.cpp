83f4fa2a00d53fea635c9e521c3ff66612ac2d5f
prayogorizkymaulany <prayogo0907@gmail.com>
2018-01-20 05:20:26 +0800

    mulai dan membuat class

---
+++ tugasclass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class buku {
+    public:
+        void jenisbuku(string buku);
+        string judulbuku;
+        void penerbitbuku(string penerbit);
+        void hargabuku(int harga);
+    
+                
+        };
