0695264e9a0dd32a34522bbff4fbb86f2e1b237a
prayogorizkymaulany <prayogo0907@gmail.com>
2018-01-20 05:37:31 +0800

    output buku

--- tugasclass.cpp
+++ tugasclass.cpp
@@ -17,3 +17,26 @@ class buku {
 	novel.judulbuku	="Garis Waktu";
 	komik.judulbuku ="One Piace ";
 	pelajaran.judulbuku ="Kewarganegaraan ";
+	
+	
+	cout << "=========== -BUKU - ==============\n";
+	cout << "novel: \n";
+	cout << (novel.judulbuku);
+	novel.penerbitbuku("Fiersa Besari");
+	novel.hargabuku( 80000 );
+	cout <<"===================================\n";
+	
+	cout << "=========== -BUKU - ==============\n";
+	cout << "komik: \n";
+	cout << (komik.judulbuku);
+	komik.penerbitbuku("Win Pisu");
+	komik.hargabuku( 90000 );
+	cout <<"===================================\n";
+	
+	cout << "=========== -BUKU - ==============\n";
+	cout << "pelajaran: \n";
+	cout << (pelajaran.judulbuku);
+	pelajaran.penerbitbuku("Erlangga");
+	pelajaran.hargabuku( 150000 );
+	cout <<"===================================\n";
+	
